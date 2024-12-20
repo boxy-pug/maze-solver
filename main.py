4cba5b8a199e771b22a8e97d1506b23c45e74dc3
boxy-pug <wewepipi99@pm.me>
2024-12-20 14:02:35 +0100

    added Maze() class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from tkinter import Tk, BOTH, Canvas
+import time
 
 class Window():
     def __init__(self, width, height):
@@ -84,10 +85,48 @@ class Cell():
 
         self._win.draw_line(Line(Point(from_x, from_y), Point(to_x, to_y)), fill)
 
-      
+class Maze():
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self._create_cells()
+
+    def _create_cells(self):
+        self._cells = []
+        for i in range(self.num_rows):
+            row = []
+            for j in range(self.num_cols):
+                cell = self._draw_cell(i, j)
+                row.append(cell)
+            self._cells.append(row)
+
+    def _draw_cell(self, i, j):
+        if self.win is None:
+            return
+        x1 = self.x1 + j * self.cell_size_x
+        y1 = self.y1 + i * self.cell_size_y
+        x2 = x1 + self.cell_size_x
+        y2 = y1 + self.cell_size_y
+        cell = Cell(self.win)
+        cell.draw(x1, y1, x2, y2)
+        self._animate()
+        return cell
+
+
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.1)
+        
 
 def main():
     win = Window(800, 600)
+
+    maze = Maze(50, 50, 5, 5, 50, 50, win)
 
     l = Line(Point(50, 50), Point(100, 100))
     win.draw_line(l, "black")
@@ -117,8 +156,6 @@ def main():
 
 
     win.wait_for_close()
-
-
 
 
 
