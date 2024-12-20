72b5f5ed8fcf6b6603d210be5e163d241d20dfed
boxy-pug <wewepipi99@pm.me>
2024-12-20 09:56:24 +0100

    fixed line and cell methods

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window():
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
         
         self.root = Tk()
         self.root.title("Maze solver")
 
-        self.canvas = Canvas(self.root, width=self.width, height=self.height)
-        self.canvas.pack()
+        self.canvas = Canvas(self.root, bg="white", width=width, height=height)
+        self.canvas.pack(fill=BOTH, expand=1)
 
         self.running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
@@ -23,8 +21,9 @@ class Window():
         self.running = True
         while self.running:
             self.redraw()
+        print("Window closed.")
 
-    def draw_line(self, line, fill_color):
+    def draw_line(self, line, fill_color="black"):
         line.draw(self.canvas, fill_color)
 
     def close(self):
@@ -41,54 +40,61 @@ class Line():
         self.point1 = point1
         self.point2 = point2
 
-    def draw(self, canvas, fill_color):
+    def draw(self, canvas, fill_color="black"):
         x1, y1 = self.point1.x, self.point1.y
         x2, y2 = self.point2.x, self.point2.y
         canvas.create_line(x1, y1, x2, y2, fill=fill_color, width=2)
 
 class Cell():
-    def __init__(self, x1, x2, y1, y2, window):
+    def __init__(self, window):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self._x1 = None# left
+        self._x2 = None# right
+        self._y1 = None# up
+        self._y2 = None# down
+        self._win = window
+
+    def draw(self, x1, x2, y1, y2):
         self._x1 = x1 # left
         self._x2 = x2 # right
         self._y1 = y1 # up
         self._y2 = y2 # down
-        self._win = window
-
-    def draw(self):
         if self.has_left_wall:
-            self._win.canvas.create_line(self._x1, self._y1, self._x1, self._y2, fill="black", width=2)
+            self._win.draw_line(Line(Point(x1, y1), Point(x1, y2)))
         if self.has_right_wall:
-            self._win.canvas.create_line(self._x2, self._y1, self._x2, self._y2, fill="black", width=2)
+            self._win.draw_line(Line(Point(x2, y1), Point(x2, y2)))
         if self.has_top_wall:
-            self._win.canvas.create_line(self._x1, self._y1, self._x2, self._y1, fill="black", width=2)
+            self._win.draw_line(Line(Point(x1, y1), Point(x2, y1)))
         if self.has_bottom_wall:
-            self._win.canvas.create_line(self._x1, self._y2, self._x2, self._y2, fill="black", width=2)
+            self._win.draw_line(Line(Point(x1, y2), Point(x2, y2)))
 
-
-
-
-
+      
 
 def main():
     win = Window(800, 600)
 
+    l = Line(Point(50, 50), Point(100, 100))
+    win.draw_line(l, "black")
 
-    cell1 = Cell(100, 100, 150, 150, win)
-    cell2 = Cell(200, 100, 250, 150, win)
-    cell3 = Cell(300, 100, 350, 150, win)
+    c1 = Cell(win)
+    c1.has_left_wall = True
+    c1.draw(50, 100, 50, 100)
 
-    cell2.has_left_wall = True  # Remove left wall
-    cell3.has_top_wall = False   # Remove top wall
-    cell3.has_bottom_wall = False  # Remove bottom wall
+    c2 = Cell(win)
+    c2.has_right_wall = True
+    c2.draw(125, 200, 125, 200)
 
+    c3 = Cell(win)
+    c3.has_bottom_wall = False
+    c3.draw(225, 225, 250, 250)
 
-    cell1.draw()
-    cell2.draw()
-    cell3.draw()
+    c4 = Cell(win)
+    c4.has_top_wall = False
+    c4.draw(300, 300, 500, 500)
+
 
 
     win.wait_for_close()
@@ -99,3 +105,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
