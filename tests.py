e68198f0c7283f4a0edf02cdc2a4466585e216e4
boxy-pug <wewepipi99@pm.me>
2024-12-20 16:05:13 +0100

    added more tests

--- tests.py
+++ tests.py
@@ -16,6 +16,19 @@ class Tests(unittest.TestCase):
             num_cols,
         )
 
+    def test_maze_different_dimensions(self):
+        test_cases = [(5, 5), (1, 1), (1, 2), (30, 20), (0, 0), (0, 10), (10, 0)]
+        for num_rows, num_cols in test_cases:
+            with self.subTest(num_rows=num_rows, num_cols=num_cols):
+                m = Maze(0, 0, num_rows, num_cols, 10, 10)
+                self.assertEqual(len(m._cells), num_rows)
+                if num_rows > 0:
+                    self.assertEqual(len(m._cells[0]), num_cols)
+
+    def test_empty_maze(self):
+        m = Maze(0, 0, 0, 0, 10, 10)
+        self.assertEqual(len(m._cells), 0)
+
 
 if __name__ == "__main__":
     unittest.main()
